bbadbfb2c09d03b3a538716509d9f4bdabbcfe79
brauliojde <bjdelato@ucsc.edu>
2017-02-28 19:28:54 -0800

    Google Maps
    including google maps on code

--- SlugRide/src/app/driver-ondemand-submit/driver-ondemand-submit.component.ts
+++ SlugRide/src/app/driver-ondemand-submit/driver-ondemand-submit.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./driver-ondemand-submit.component.css']
 })
 export class DriverOndemandSubmitComponent implements OnInit {
+  title: string = "Google Maps";
+  lat: number = 51.678418;
+  lng: number = 7.809007;
 
   constructor() { }
 
