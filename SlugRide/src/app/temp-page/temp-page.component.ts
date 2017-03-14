341cea41bfe8c48d97cef30198e80f7d418443e2
andrewjdato <adato@ucsc.edu>
2017-03-13 23:07:38 -0700

    Temp Page Change
    Added correct JSON model

--- SlugRide/src/app/temp-page/temp-page.component.ts
+++ SlugRide/src/app/temp-page/temp-page.component.ts
@@ -4,13 +4,22 @@ import { Router } from '@angular/router';
  
 import { UserService } from '../_services/user.service';
 
+export class Temp {
+  name : string;  
+  description : string;
+  slogan : string;
+  founded_date : string;
+  website : string;
+}
+
+
 @Component({
   selector: 'app-temp-page',
   templateUrl: './temp-page.component.html',
   styleUrls: ['./temp-page.component.css']
 })
 export class TempPageComponent implements OnInit {
-  model: any = {};
+  model: Temp;
 
 
   constructor(
@@ -18,6 +27,14 @@ export class TempPageComponent implements OnInit {
         private userService: UserService) { }
 
   ngOnInit() {
+        this.model = {
+          name: 'Wrong',
+          description: 'Wrong', 
+          slogan: 'Wrong', 
+          founded_date: 'Wrong', 
+          website: 'Wrong',  
+        }
+
     this.userService.tempFunc().subscribe(model => { this.model = model; });
   }
 
