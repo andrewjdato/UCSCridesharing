81c586fca608652a7f1284ee93ee6c350ac0ad54
DimitriGerasimov <dimitrigerasim@gmail.com>
2017-03-15 00:21:19 -0700

    Login working I think, v1

--- server/rideshare/serializers.py
+++ server/rideshare/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 
-class UserRegSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name', 'email', 'password',)
@@ -21,6 +21,6 @@ class UserRegSerializer(serializers.ModelSerializer):
 class UserLoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'password',)
+        fields = ('email', 'password',)
         
         
--- server/rideshare/views.py
+++ server/rideshare/views.py
@@ -1,27 +1,33 @@
 from rest_framework import viewsets
-from rideshare.serializers import UserRegSerializer
+from rideshare.serializers import UserSerializer, UserLoginSerializer
 from django.contrib.auth.models import User
 
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from django.contrib.auth import authenticate
 
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def user_registration(request):
-    if request.method =='POST':
-        serializer = UserRegSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.validated_data['username'] = serializer.validated_data['email']
-            serializer.save()
-            return Response(serializer.data, status=201)
-        print("not valid")
-        return Response(serializer.errors, status=400)
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.validated_data['username'] = serializer.validated_data['email']
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
         
-@api_view(['GET'])
+@api_view(['POST'])
+@parser_classes((JSONParser,))
 def user_login(request):
-    if request.method =='GET':
-        serializer = UserLoginSerializer(data=request.data)
-        if serializer.is_valid():
-            print(serializer.validated_data)
+    json_data = JSONParser().parse(request)
+    serializer = UserLoginSerializer(data=json_data)
+    print(json_data)
+    if serializer.is_valid():
+        try:
+            user = authenticate(username=serializer.validated_data['email'], password=serializer.validated_data['password'])
+            serializer = UserSerializer(user)
             return Response(serializer.data, status=201)
+        except User.DoesNotExist:
+            return Response(serializer.data, status=400)
