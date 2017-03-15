dbbf7827cb279f3d9c13d7ac28bbc480fa8fa2c1
andrewjdato <adato@ucsc.edu>
2017-03-14 17:57:25 -0700

    User Model Change
    Model Change

--- SlugRide/src/app/_user/user.ts
+++ SlugRide/src/app/_user/user.ts
@@ -1,7 +1,6 @@
 export class User {
-    firstName: string;
-    lastName: string;
+    firstname: string;
+    lastname: string;
     email: string;
     password: string;
-    confirm_password: string; 
 }
