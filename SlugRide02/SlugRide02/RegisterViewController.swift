fb56a3ec124556c006512a222f05a08852fccac5
brauliojde <bjdelato@ucsc.edu>
2017-04-17 17:06:57 -0700

    Add files via upload

---
+++ SlugRide02/SlugRide02/RegisterViewController.swift
@@ -0,0 +1,14 @@
+
+//
+//  LoginViewController.swift
+//  SlugRide02
+//
+//  Created by Braulio De La Torre on 4/17/17.
+//  Copyright © 2017 SDP2017SlugRide. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+}
